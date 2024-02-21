6f5beaa06d2202695e9be095b899fda23a7b2f72
Victor-Pausan <pausanvictor@gmail.com>
2024-02-21 02:48:42 +0200

    added new section with slideshow and js file

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const buttons = document.querySelectorAll("[btn-slider]")
+var index = 0;
+
+function buttonClick(btnClicked)
+{
+    let offset;
+    if(btnClicked === "btn-left")
+    {
+        offset = -1;
+    }
+    else
+    {
+        offset = 1;
+    }
+
+    const slides = document.querySelectorAll(".slide");
+    const lenght = slides.length;
+
+    for(let i = 0; i < lenght; i++)
+    {
+        delete slides[i].dataset.active;
+    }
+
+    index += offset;
+    if(index > lenght-1)
+    {
+        index = 0;
+    }
+    if(index < 0)
+    {
+        index = lenght-1;
+    }
+    
+    slides[index].dataset.active = true;
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click", () =>{
+        buttonClick(button.className);   
+    })
+});
