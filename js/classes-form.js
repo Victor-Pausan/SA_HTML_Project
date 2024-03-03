9c3492012b5df750a1f26d9051c68deec922e2a8
Victor-Pausan <pausanvictor@gmail.com>
2024-03-03 23:28:47 +0200

    REST api with node express, ability to make an appointment and delete it

--- js/classes-form.js
+++ js/classes-form.js
@@ -9,6 +9,7 @@ form.addEventListener("submit", (event) => {
     const name = form.querySelector("#name").value;
 
     const formData = {
+        id: "1",
         location: location,
         date: date,
         time: time,
@@ -18,5 +19,98 @@ form.addEventListener("submit", (event) => {
     if (location.trim() === '' || date.trim() === '' || time.trim() === '' || name.trim() === '') {
         alert("Please fill in all fields.")
         return;
+    }
+
+    postData(formData);
+
+    form.submit();
+})
+
+function postData(appointment) {
+    fetch("http://localhost:8080/getAppointments")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    })
+
+    fetch("http://localhost:8080/addAppointment", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(appointment)
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        })
+}
+
+function updateAppointmentsList(){
+    let schedule = document.getElementById('schedule');
+    fetch("http://localhost:8080/getAppointments")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+    })
+    .then((data) => {
+        Object.keys(data || {}).forEach(key => {
+            const value = data[key];
+            let i = value.id;
+            schedule.innerHTML += `<div id="app${i}"class="note">
+            <h5>Name: ${data[`app${i}`].name}</h5>
+            <h5>Date and time: ${data[`app${i}`].date} at ${data[`app${i}`].time}</h5>
+            <h5>Location: ${data[`app${i}`].location}</h5>
+            <button id="b${i}" class="delete erase">
+                <svg id="b${i}" class="erase" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+            </button>
+            </div>`;
+        });
+    });
+}
+
+updateAppointmentsList();
+
+document.addEventListener('click', event => {
+    if (event.target.classList.contains('erase')) {
+        let param = event.target.id.slice(1);
+        console.log(param);
+        fetch(`http://localhost:8080/deleteAppointment/:${param}`, {
+            method: 'DELETE'
+        })
+        .then((response) => {
+            if(!response.ok){
+                throw new Error("Response is not ok.")
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            const element = documen.getElementById(`b${param}`);
+            element.remove();
+        })
+        .catch((error) => {
+            console.error("Error", error);
+        })
     }
 })
---
+++ node/server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var app = express();
+const port = 8080;
+const cors = require('cors');
+var fs = require('fs');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/getAppointments', (req, res) => {
+    fs.readFile(__dirname + "/" + "appointments.json", 'utf8', (err, data) =>  {
+        console.log(data);
+        res.end(data);
+    })
+})
+
+app.post('/addAppointment', (req, res) => {
+    fs.readFile(__dirname + "/" + "appointments.json", 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        let appointments = JSON.parse(data) || {};
+        let index = Object.keys(appointments).length+1;
+        while(appointments["app"+index]){
+            index++;
+        }
+        const newAppointment = req.body;
+        newAppointment.id = index.toString();
+        console.log(newAppointment);
+        appointments[`app${newAppointment.id}`] = newAppointment;
+
+        fs.writeFile(__dirname + "/" + 'appointments.json', JSON.stringify(appointments), 'utf-8', (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Internal Server Error' });
+                return;
+            }
+
+            res.json(appointments);
+        })
+    })    
+})
+
+app.delete('/deleteAppointment/:id', (req, res) => {
+    fs.readFile(__dirname +"/" + "appointments.json", "utf-8" ,(err, data) => {
+        let appointments = JSON.parse(data);
+        delete appointments["app"+ req.params.id.slice(1)];
+        console.log("app"+ req.params.id.slice(1)); 
+    })
+
+    fs.writeFile(__dirname + "/" + 'appointments.json', JSON.stringify(appointments), 'utf-8', (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+        res.json(appointments);
+    })
+})
+
+var server = app.listen(port, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("REST API demo app listening at http://localhost:", port);
+})
