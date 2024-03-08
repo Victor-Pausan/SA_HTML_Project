7bb95392408f0f0f737e4c3b04d98a4e714b3e29
Victor-Pausan <pausanvictor@gmail.com>
2024-03-09 00:51:25 +0200

    added bootstrap to landing page content and replaced slideshow

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
-const buttons = document.querySelectorAll("[btn-slider]")
-var index = 0;
+// const buttons = document.querySelectorAll("[btn-slider]")
+// var index = 0;
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        buttonClick(button.className);
-    })
-});
+// buttons.forEach(button => {
+//     button.addEventListener("click", () => {
+//         buttonClick(button.className);
+//     })
+// });
 
-function buttonClick(btnClicked) {
-    let offset;
-    if (btnClicked === "btn-left") {
-        offset = -1;
-    }
-    else {
-        offset = 1;
-    }
+// function buttonClick(btnClicked) {
+//     let offset;
+//     if (btnClicked === "btn-left") {
+//         offset = -1;
+//     }
+//     else {
+//         offset = 1;
+//     }
 
-    const slides = document.querySelectorAll(".slide");
-    const lenght = slides.length;
+//     const slides = document.querySelectorAll(".slide");
+//     const lenght = slides.length;
 
-    for (let i = 0; i < lenght; i++) {
-        delete slides[i].dataset.active;
-    }
+//     for (let i = 0; i < lenght; i++) {
+//         delete slides[i].dataset.active;
+//     }
 
-    index += offset;
-    if (index > lenght - 1) {
-        index = 0;
-    }
-    if (index < 0) {
-        index = lenght - 1;
-    }
+//     index += offset;
+//     if (index > lenght - 1) {
+//         index = 0;
+//     }
+//     if (index < 0) {
+//         index = lenght - 1;
+//     }
 
-    slides[index].dataset.active = true;
-}
+//     slides[index].dataset.active = true;
+// }
 
-async function autoChangeSlide() {
-    while (true) {
-        await new Promise((resolve) => setTimeout(resolve, 3500));
-        buttonClick("btn-right");
-    }
-}
+// async function autoChangeSlide() {
+//     while (true) {
+//         await new Promise((resolve) => setTimeout(resolve, 3500));
+//         buttonClick("btn-right");
+//     }
+// }
 
-autoChangeSlide();
+// autoChangeSlide();
 
 //more elegant way of doing what the autoChangeSlide() func does
 //
